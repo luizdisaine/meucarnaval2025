bbe5833e475a98b84aac8f6620854fa3a9ad6adc
luizdisaine <47760793+luizdisaine@users.noreply.github.com>
2026-02-04 13:18:24 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <?php
 get_header(); // Include the header.php file
+?>
 
-if ( have_posts() ) :
+<div class="container-fluid bg-amarelo py-5" id="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9 mx-auto">
+            <?php if ( have_posts() ) :
     while ( have_posts() ) : the_post(); ?>
         <h2><?php the_title(); ?></h2>
         <div><?php the_content(); ?></div>
     <?php endwhile;
 else :
     echo '<p>No content found</p>';
-endif;
+endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
 
+<?php 
 get_footer(); // Include the footer.php file
 ?>
